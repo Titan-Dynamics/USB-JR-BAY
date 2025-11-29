b53bf657b9b2c872ad0655c4f5b9c792039f2055
wvarty <wezley.varty@gmail.com>
2025-11-29 21:15:25 +1000

    Fix bug with toggle and rotary not deselecting when changing from button to axis

--- tools/feeder/channel_ui.py
+++ tools/feeder/channel_ui.py
@@ -257,6 +257,16 @@ class ChannelRow(QtWidgets.QWidget):
         if is_axis:
             self.toggleBox.setStyleSheet("color: gray;")
             self.rotaryBox.setStyleSheet("color: gray;")
+            # Uncheck toggle and rotary if not a button source
+            if self.toggleBox.isChecked() or self.rotaryBox.isChecked():
+                self.toggleBox.blockSignals(True)
+                self.rotaryBox.blockSignals(True)
+                self.toggleBox.setChecked(False)
+                self.rotaryBox.setChecked(False)
+                self.toggleGroupBox.setEnabled(False)
+                self.toggleBox.blockSignals(False)
+                self.rotaryBox.blockSignals(False)
+                self.changed.emit()
         else:
             self.toggleBox.setStyleSheet("")
             self.rotaryBox.setStyleSheet("")
