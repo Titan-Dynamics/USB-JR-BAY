c6275bb507116275592f54bc95d592fbd2e672e3
wvarty <wezley.varty@gmail.com>
2026-05-13 19:37:09 +1000

    Remove dead esp-bridge code

--- test/test_crsf_task/Arduino.cpp
+++
@@ -1,9 +0,0 @@
-/**
- * @file Arduino.cpp
- * @brief Arduino mock implementation
- */
-
-#include "Arduino.h"
-
-// Define global Serial mock
-MockSerial Serial;
--- test/test_crsf_task/timing_mock.cpp
+++
@@ -1,9 +0,0 @@
-/**
- * @file timing_mock.cpp
- * @brief Timing mock implementation
- */
-
-#include "timing_mock.h"
-
-// Define global timing mock
-TimingMock timingMock;
